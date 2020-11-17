a7ca3e94cc51d4fdc6b3da2d5feff1bc22458e6f
m4stersplint3r <m4stersplint3r@gmail.com>
2020-11-17 03:18:45 -0500

    first commit

---
+++ CountriesStateManagement/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using CountriesStateManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CountriesStateManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<Country> countries = new List<Country>{
+            new Country("USA", "English", "Hello World", "The land of the free", new List<string> { "red", "white", "blue" }),
+            new Country("Mexico", "Espanol", "Hola amigo.", "This is Mexico", new List<string> { "green", "white", "red" }),
+            new Country("Japan", "Japanese", "Kon'nichiwa tomodachi.", "All about Japan", new List<string> { "red", "white"}),
+            };
+
+        public IActionResult Index()
+        {
+            return View(countries);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CountryDetails(int index)
+        {
+            Country country = countries[index];
+            return View(country);
+        }
+
+        public IActionResult CountryDescription()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CountriesStateManagement/Models/Country.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CountriesStateManagement.Models
+{
+    public class Country
+    {
+        public string Name { get; set; }
+        public string Language { get; set; }
+        public string Greeting { get; set; }
+        public string Description { get; set; }
+        public List<string> NationalColors { get; set; }
+
+        public Country(string name, string language, string greeting, string description, List<string> nationalColors)
+        {
+            Name = name;
+            Language = language;
+            Greeting = greeting;
+            Description = description;
+            NationalColors = nationalColors;
+        }
+    }
+}
